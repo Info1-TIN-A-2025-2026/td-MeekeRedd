964abae08da589615eb89d15cdb4c63636e18c2d
baschiera.michele <michele.baschiera@heig-vd.ch>
2025-09-16 12:32:31 +0000

    added new TD labo 20250916

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	unsigned int num_students = 23; //unsigned whole number of students
+	//signed int m = -42; //signed is optional
+	int account_value = -42; //signed whole number for the account value
+	float temperature = 18.1; //floating decimal number 32 bit
+	double pi = 3.1415; //floating decimal number 64bit
+}
