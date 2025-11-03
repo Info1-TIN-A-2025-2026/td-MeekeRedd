c6aca23c7e9245e66c384289130b2831c9710abb
baschiera.michele <michele.baschiera@heig-vd.ch>
2025-11-03 13:54:03 +0000

    implemented

--- TD20251103/td.c
+++ TD20251103/td.c
@@ -4,27 +4,30 @@
 int main(int argc, const char *argv[])
 {
 
-/* 	const unsigned int BASE = 10;
+	/* 	const unsigned int BASE = 10;
 
-	unsigned int n = atoi(argv[1]); // n>0 not tested
-	unsigned int w = 0;	
-	
-	while (n > 0)
-	{
-		n /= BASE;
-		w++;
-	}
+		unsigned int n = atoi(argv[1]); // n>0 not tested
+		unsigned int w = 0;
 
-	printf("w: %d\n", w); */
+		while (n > 0)
+		{
+			n /= BASE;
+			w++;
+		}
+
+		printf("w: %d\n", w); */
 
 	unsigned int n = atoi(argv[1]);
 	unsigned int w = 0;
 
 	do
 	{
-		w += 10;
-		w += n % 10;
-		n /= 10;
+		//each loop multiply with 10 and add the modulus
+		w = w * 10;
+		w = w + (n % 10); // modulo return the reminder from the division
+
+		//each loop devide by 10
+		n = n / 10;
 
 	} while (n > 0);
 	printf("w=%u\n", w);
