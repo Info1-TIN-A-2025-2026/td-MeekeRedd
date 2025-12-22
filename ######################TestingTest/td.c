34b16d7531a46c839b0f1b0462f64b138e380c34
baschiera.michele <michele.baschiera@heig-vd.ch>
2025-12-22 20:18:06 +0000

    practice

--- ######################TestingTest/td.c
+++ ######################TestingTest/td.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
-int test(const int n);
+
+int test2(const int n);
+int test(const char *str);
 
 int main(int argc, const char *argv[])
 {
-	test(12);
+	//test(12);
+	test("prerfasdeokinbvgtedoesirefa");
 	return 0;
 }
 
-int test(const int n) // find perfect numbers
+int test2(const int n) // find perfect numbers
 {
 	int sum = 0;
 
@@ -30,4 +34,38 @@ int test(const int n) // find perfect numbers
 		return 1;
 
 	return 0;
+}
+
+int test(const char *str)
+{
+
+	char vowels[10] = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+	int amount = 0;
+	if (strcmp(str, "NULL") == 0)
+	{
+		return -1;
+	}
+	else if (strcmp(str, "") == 0)
+	{
+		return 0;
+	}
+	//printf("size of letter %ld\n", strlen(str));
+	for (int let = 0; let < strlen(str); let++)
+	{
+
+		//printf("shit letter %c\n", str[let]);
+		for (int vow = 0; vow < 10; vow++)
+		{
+		if (vowels[vow] == str[let])
+		{
+			//printf("fucking vowels %c\n", vowels[vow]);
+			amount++;
+		}
+		}
+		
+
+	}
+
+	//printf("amount = %d\n", amount);
+	return amount;
 }
